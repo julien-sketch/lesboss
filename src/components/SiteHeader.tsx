59af0b2f819a67f614f4cf99d4867d6e073fb7d3
julien-sketch <julien@picqtures.fr>
2026-08-05 10:34:56 +0200

    Optimise src/components/SiteHeader.tsx

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -59,7 +59,7 @@ export function SiteHeader() {
 
         <div className="hidden sm:block">
           <ButtonLink href="/evenements/tour-du-monde-des-boss">
-            Réserver
+            Voir l&apos;événement
           </ButtonLink>
         </div>
 
@@ -98,7 +98,7 @@ export function SiteHeader() {
             href="/evenements/tour-du-monde-des-boss"
             className="mt-2 w-full"
           >
-            Découvrir l&apos;événement
+            Voir l&apos;événement du 22 septembre
           </ButtonLink>
         </div>
       </div>
